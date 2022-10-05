bafeede318a2b77c37ca396edfff882370b4c88a
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-10-05 09:00:07 -0300

    feat: add content homepage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,5 +1,24 @@
+import logo from "../../img/Logo.png";
+
 const HomePage = () => {
-  return <h1>Sou a HOME</h1>;
+  return (
+    <div className="homePage">
+      <nav>
+        <img src={logo} alt="Logo Kenzie Hub" />
+        <button>Sair</button>
+      </nav>
+      <header>
+        <h1>Olá, Samuel Leão!</h1>
+        <span>Primeiro módulo (Introdução ao Frontend)</span>
+      </header>
+      <main>
+        <h2>Que pena! Estamos em desenvolvimento :(</h2>
+        <p>
+          Nossa aplicação está em desenvolvimento, em breve teremos novidades
+        </p>
+      </main>
+    </div>
+  );
 };
 
 export default HomePage;
