93bd1bfe7f8d2bbf336e96db15fceffde14aedc8
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-10-20 17:51:07 -0300

    feat: install typescript dependences

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,9 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import Global from "./styles/global";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
