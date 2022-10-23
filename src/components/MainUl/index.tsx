ce09eb519436b4e1349db23043ba97efba36eae3
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-10-23 13:48:24 -0300

    feat: change MainUl to TS

--- src/components/MainUl/index.tsx
+++ src/components/MainUl/index.tsx
@@ -1,22 +1,21 @@
-import { useContext } from "react";
-import { TechContext } from "../../contexts/TechContext";
+import { useTechContext } from "../../contexts/TechContext";
 import { useUserContext } from "../../contexts/UserContext";
 import { StyledMainUl } from "./style";
 
 const MainUl = () => {
   const { setTechDetails, user } = useUserContext();
-  const { setIdLi, setNameTech } = useContext(TechContext);
+  const { setIdLi, setNameTech } = useTechContext();
 
   return (
     <StyledMainUl>
-      {user.techs?.map((technologies) => (
+      {user?.techs?.map((technologies) => (
         <li
           id={technologies.id}
           key={technologies.id}
-          onClick={(e) => {
+          onClick={() => {
             setTechDetails(true);
-            setNameTech(e.target.children[0].id);
-            setIdLi(e.target.id);
+            setNameTech(technologies.title);
+            setIdLi(technologies.id);
           }}
         >
           <p id={technologies.title}>{technologies.title}</p>
